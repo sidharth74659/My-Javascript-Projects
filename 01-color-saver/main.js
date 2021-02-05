b04d9b081e64c2a9195c3b10a38a9475ba624715
sidharth74659 <sidharthsid74659@gmail.com>
2021-02-05 23:14:51 +0530

    updated UI and refactored code
    Ui Design from Dojo

--- 01-color-saver/main.js
+++ 01-color-saver/main.js
@@ -1,17 +1,22 @@
 const form = document.querySelector(".form");
-const list = document.querySelector(".list");
+const color_list = document.querySelector(".color-list");
 const colorArray = JSON.parse(localStorage.getItem("colorArray")) || [];
 const input = document.querySelector(".input");
-// loads existing Colors from localStorage or loads none
-populateList(colorArray, list);
 
-function addItem(e) {
-  e.preventDefault();
+// Step 01 :  loads existing Colors from localStorage or loads none on StartUp
+populateList(colorArray, color_list);
+
+// Step 02 (on-clicking 'add'):  add item to Display and to localStorage
+form.addEventListener("submit", addColor);
+
+function addColor(event) {
+  // console.log(this)
+  event.preventDefault();
   const colorName = input.value;
   const item = { colorName };
   //TODO: for color-names: validateColor(colorName);
   colorArray.push(item);
-  populateList(colorArray, list);
+  populateList(colorArray, color_list);
   localStorage.setItem("colorArray", JSON.stringify(colorArray));
   form.reset();
 }
@@ -22,18 +27,14 @@ function populateList(colorArray, list) {
   list.innerHTML = uniqueColors
     .map((color, i) => {
       return `
-       <div class="color__block">
-          <span style="background:${color.colorName}" class="color__box">
-         
-          </span>
-          
-          <input type="text" data-index=${i} class="color__text" value="${color.colorName}" >
-        </div>`;
+      <li class="color-item"  style="background:${color.colorName}">
+          <input type="text" data-index=${i} class="color__text" readonly value="${color.colorName}">
+        </li>
+       `;
     })
     .join("");
 }
 
-form.addEventListener("submit", addItem);
 // Found in stackoverflow https://stackoverflow.com/questions/2218999/remove-duplicates-from-an-array-of-objects-in-javascript#answer-56768137:~:text=function%3A-,function%20getUniqueListBy(arr%2C%20key)%20%7B,%7D
 
 function getUniqueListBy(arr, key) {
@@ -41,21 +42,30 @@ function getUniqueListBy(arr, key) {
 }
 
 //TODO :  function validateColor(colorName) {}
-  //TODO :Alert the copied text 
+//TODO :Alert the copied text
 function copyColor(index) {
+  // let copyText = document.querySelector(`[data-index='${index}']`);
+  // copyText.select();
+  // document.execCommand("copy");
   let copyText = document.querySelector(`[data-index='${index}']`);
+  // console.log(copyText);
   copyText.select();
   document.execCommand("copy");
 
-
+  copied.classList.toggle("is-copied");
+  setTimeout(() => copied.classList.remove("is-copied"), 1000);
 }
 
-const color__block = document.querySelector(".color__box");
-list.addEventListener("click", function (e) {
-  if (!e.target.matches("span")) return;
-  //Doesn't work:?:  console.log(e.target.nextSibling);
+const copied = document.querySelector(".copied");
+// const color__block = document.querySelector(".color__box");
+color_list.addEventListener("click", function (e) {
   // console.log(e.target.parentNode.childNodes[3].attributes[1]);
-  const index = e.target.parentNode.children[1].dataset.index
-  copyColor(index);
+  // console.log(e.target.parentNode.children[1].children[0]);
+  // console.log(e.target.children[0].dataset.index);
+  if (!e.target.matches("li")) return;
 
+  //e.target.nextSibling Doesn't work ?
+
+  const index = e.target.children[0].dataset.index;
+  copyColor(index);
 });
