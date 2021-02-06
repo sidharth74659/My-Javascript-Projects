322cead7b3d1c4953400e68bd4c42f7e82f89995
sidharth74659 <sidharthsid74659@gmail.com>
2021-02-06 23:16:46 +0530

    documented and refactored code

--- 01-color-saver/main.js
+++ 01-color-saver/main.js
@@ -1,71 +1,70 @@
+//TODO: for color-names: validateColor(colorName);
+
+
 const form = document.querySelector(".form");
+const input = document.querySelector(".input");
 const color_list = document.querySelector(".color-list");
 const colorArray = JSON.parse(localStorage.getItem("colorArray")) || [];
-const input = document.querySelector(".input");
+const copied = document.querySelector(".copied");
 
 // Step 01 :  loads existing Colors from localStorage or loads none on StartUp
 populateList(colorArray, color_list);
 
-// Step 02 (on-clicking 'add'):  add item to Display and to localStorage
+// Step 02 (on-clicking 'add'):  add item on Display and to localStorage
 form.addEventListener("submit", addColor);
 
+// Step 03 (on-hover and 'click'):  copy colorCode of that item
+color_list.addEventListener("click", copyColor)	// Event Delegation
+
 function addColor(event) {
-  // console.log(this)
-  event.preventDefault();
-  const colorName = input.value;
-  const item = { colorName };
-  //TODO: for color-names: validateColor(colorName);
-  colorArray.push(item);
-  populateList(colorArray, color_list);
-  localStorage.setItem("colorArray", JSON.stringify(colorArray));
-  form.reset();
+	event.preventDefault(); //prevent form from refreshing
+	// const colorName = input.value;  
+	const color = { colorValue: input.value };  //store colorValue 
+	colorArray.push(color);   //update colorArray with the latest color
+	localStorage.setItem("colorArray", JSON.stringify(colorArray)); //update localStorage with latest color 
+	populateList(colorArray, color_list); //update the color onto Display
+	form.reset();
 }
 
 function populateList(colorArray, list) {
-  const uniqueColors = getUniqueListBy(colorArray, "colorName");
+	// filter colors that have same values
+	const uniqueColors = getUniqueListBy(colorArray, "colorValue");
 
-  list.innerHTML = uniqueColors
-    .map((color, i) => {
-      return `
-      <li class="color-item"  style="background:${color.colorName}">
-          <input type="text" data-index=${i} class="color__text" readonly value="${color.colorName}">
-        </li>
+	// load the page with colors that are filtered from localStorage 
+	list.innerHTML = uniqueColors
+		.map((color, i) => {
+			return `
+				  <li class="color-item"  style="background:${color.colorValue}">
+				  <input type="text" data-index=${i} class="color__text" readonly value="${color.colorValue}">
+				</li>
        `;
-    })
-    .join("");
+		})
+		.join("");
+}
+
+function copyColor(eventObj) {
+	// check if the user clicked one of the children 
+	if (!eventObj.target.matches("li")) return;
+
+	//Why: e.target.nextSibling Doesn't work ?
+
+	// select the user-clicked element and copy its value using its unique data-index value
+	let copyText = document.querySelector(`[data-index='${getColorIndex(eventObj)}']`);
+	copyText.select();
+	document.execCommand("copy");
+	// notify-user wwith a pop-up
+
+	// clearTimeout(timeOut);
+	copied.classList.toggle("is-copied");
+	let timeOut = setTimeout(() => copied.classList.remove("is-copied"), 2000);
 }
 
 // Found in stackoverflow https://stackoverflow.com/questions/2218999/remove-duplicates-from-an-array-of-objects-in-javascript#answer-56768137:~:text=function%3A-,function%20getUniqueListBy(arr%2C%20key)%20%7B,%7D
-
 function getUniqueListBy(arr, key) {
-  return [...new Map(arr.map((item) => [item[key], item])).values()];
+	return [...new Map(arr.map((item) => [item[key], item])).values()];
 }
 
-//TODO :  function validateColor(colorName) {}
-//TODO :Alert the copied text
-function copyColor(index) {
-  // let copyText = document.querySelector(`[data-index='${index}']`);
-  // copyText.select();
-  // document.execCommand("copy");
-  let copyText = document.querySelector(`[data-index='${index}']`);
-  // console.log(copyText);
-  copyText.select();
-  document.execCommand("copy");
-
-  copied.classList.toggle("is-copied");
-  setTimeout(() => copied.classList.remove("is-copied"), 1000);
+// picks up the data-index of the element the user-clicked
+function getColorIndex(e) {
+	return e.target.children[0].dataset.index
 }
-
-const copied = document.querySelector(".copied");
-// const color__block = document.querySelector(".color__box");
-color_list.addEventListener("click", function (e) {
-  // console.log(e.target.parentNode.childNodes[3].attributes[1]);
-  // console.log(e.target.parentNode.children[1].children[0]);
-  // console.log(e.target.children[0].dataset.index);
-  if (!e.target.matches("li")) return;
-
-  //e.target.nextSibling Doesn't work ?
-
-  const index = e.target.children[0].dataset.index;
-  copyColor(index);
-});
