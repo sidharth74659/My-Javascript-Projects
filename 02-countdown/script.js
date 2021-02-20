843a70908b2b0c9b92459ef295b08a7207b66e1c
sidharth74659 <sidharthsid74659@gmail.com>
2021-02-21 01:17:42 +0530

    second project

---
+++ 02-countdown/script.js
@@ -0,0 +1,156 @@
+const mins__count = document.querySelector('.mins__count');
+const seconds__count = document.querySelector('.seconds__count');
+
+const start = document.querySelector('.start');
+const reset = document.querySelector('.reset');
+
+var time = document.forms[0].elements['radio'].value;
+
+
+let secondInterval;
+let second = 60;
+let minute = time;
+
+
+
+document.querySelector('.timer').addEventListener('change', () => {
+
+    globalThis.time = document.forms[0].elements['radio'].value;
+    mins__count.innerHTML = time;
+})
+mins__count.innerHTML = time;
+seconds__count.innerHTML = "00";
+
+start.addEventListener('click', startTimer);
+reset.addEventListener('click', stopTimer);
+
+function startTimer() {
+    minute = time;
+
+    start.disabled = true;
+    reset.disabled = false;
+
+    // seconds();
+
+    document.querySelectorAll('.timer .input').forEach((input) => {
+        // console.log(input);
+        if (!input.disabled) {
+            input.disabled = true;
+        }
+
+    })
+
+    secondInterval = setInterval(seconds, 1000);
+
+}
+function stopTimer() {
+
+    second = 3;
+    minute = time;
+
+    mins__count.innerHTML = minute;
+    seconds__count.innerHTML = 0;
+
+    start.disabled = false;
+    reset.disabled = true;
+
+    document.querySelectorAll('.timer .input').forEach((input) => {
+        console.log(input);
+        if (input.disabled) {
+            input.disabled = false;
+        }
+    })
+
+
+    clearInterval(secondInterval);
+
+}
+
+// console.log(second, "seconds");
+function seconds() {
+    second--;
+    // console.log(second);
+    if (second < 0) {
+        second = 59;
+        // console.log(typeof +minute);
+        // console.log(typeof +minute, "increment");
+        minute = +minute - 1;
+        if (minute < 0) {
+            stopTimer();
+            return false;
+        }
+        mins__count.innerHTML = minute;
+    }
+
+    seconds__count.innerHTML = second;
+    console.log({ minute, second });
+}
+
+// console.log(time);
+// console.log(document.forms[0].elements['radio'].value);
+
+
+// Choose the "red" option
+// radios.value = 'red';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const date = new Date();
+
+// // console.log(date.getDate());
+// // console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+// const secondInterval = setInterval(seconds, 1000)
+
+// let second = 5;
+// let minute = 2;
+// document.querySelector('.mins__count').innerHTML = minute
+// document.querySelector('.seconds__count').innerHTML = second
+
+// function seconds() {
+//     if (second < 0) {
+//         if (minute = 0) {
+//             document.querySelector('.mins__count').innerHTML = 00;
+//             document.querySelector('.seconds__count').innerHTML = 00;
+//             clearInterval(secondInterval)
+//             return
+//         }
+//         minute--;
+//         // finally an Explanation i=on diffrence between ++i vs --i
+//         console.log({minute});
+//         document.querySelector('.mins__count').innerHTML = minute;
+//         second = 5;
+//     }
+//     document.querySelector('.seconds__count').innerHTML = second--
+// }
+
+// function startTimer() {
+
+// }
+// function stopTimer() {
+
+// }
